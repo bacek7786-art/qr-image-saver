602233b212564feab8ba157a3996e05f0f7bf4d8
acek <bacek7786@gmail.com>
2026-01-20 20:01:07 +0900

    Fix admin panel to use real API

---
+++ functions/api/icons.js
@@ -0,0 +1,24 @@
+// GET /api/icons - Test endpoint (static response)
+export async function onRequestGet(context) {
+  return new Response(JSON.stringify({
+    success: true,
+    message: "Icons API is working"
+  }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    }
+  });
+}
